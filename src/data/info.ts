b619234d5960bcb90633f91d3f43cf4556581656
Md Anjum Kamali <mdanjumkamali@gmail.com>
2024-01-09 00:36:58 +0530

    portfolio

---
+++ src/data/info.ts
@@ -0,0 +1,44 @@
+export const info = {
+  baseUrl: "https://astro-portfolio-uzair.vercel.app",
+  name: "Md Anjum Kamali",
+  jobDescription: "Web Developer",
+  about: `I'm a Full Stack based in India.
+   I enjoy creating things that live on the internet, whether that be websites, applications, or anything in between.
+I have experience in HTML, CSS, SCSS, JavaScript, Typescript, tailwind CSS, React JS, and Next JS. Hence,
+   I have successfully completed several web projects from scratch. 
+   Yet, I learn continuously and never stop exploring new technologies.
+   So, I have also gained knowledge of Node JS, Express JS, Mongo DB, and Python with time.
+  `,
+
+  socialMedia: {
+    facebook: "",
+    twitter: "https://twitter.com/md_anjum_kamali",
+    github: "https://github.com/mdanjumkamali",
+    email: "mailto:mdanjumkamaligmail.com",
+    linkedin: "https://www.linkedin.com/in/mdanjumkamali/",
+  },
+
+  projects: [
+    {
+      title: "Chatbot",
+      isFeatured: true,
+      thumbnail: "/assets/images/chatbot.png",
+      githubUrl: "https://github.com/mdanjumkamali/chatbot",
+      liveUrl: "https://github.com/mdanjumkamali/chatbot",
+    },
+    {
+      title: "Issue Tracker",
+      isFeatured: true,
+      thumbnail: "/assets/images/issuetracker.png",
+      githubUrl: "https://github.com/mdanjumkamali/issue-tracker",
+      liveUrl: "https://github.com/mdanjumkamali/issue-tracker",
+    },
+    {
+      title: "Game Hub",
+      isFeatured: true,
+      thumbnail: "/assets/images/gamehub.png",
+      githubUrl: "https://github.com/mdanjumkamali/game-hub",
+      liveUrl: "https://game-hub-4u.vercel.app/",
+    },
+  ],
+};
